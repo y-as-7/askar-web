f187ebd81f309bc36d4b18900c64913b963bf495
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-01-30 14:58:31 +0200

    last update

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-let coursesBtn = document.querySelector("header .links button");
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
 
-let list =document.querySelectorAll("header .links .list a ")
-coursesBtn.onclick=()=>{
-    
-    list.forEach((item)=>{
-        item.style.display="unset"
-    })
-}
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
