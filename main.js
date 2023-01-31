9a05f6e8ceabf06b5194c9b5088fbcec8b0b9665
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-01-31 17:54:41 +0200

    updating on project section

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const data = [
 ];
 
 
-//loop on my data
+//loop on my data on course section
 let courses_contsiner = document.querySelector(".courses-cards");
 
 for (let i = 0; i < data.length; i++) {
@@ -50,9 +50,52 @@ for (let i = 0; i < data.length; i++) {
                 <div class="card-body">
                     <h1>${data[i].title}</h1>
                     <p>${data[i].info}</p>
-                    <button>start learn</button>
+                    <button>start tour</button>
                 </div>
             </div>
     `;
     courses_contsiner.innerHTML+=card;
 }
+
+
+// projects filter 
+let btns = document.querySelectorAll("#projects .buttons button");
+let imgs = document.querySelectorAll("#projects .cards img");
+
+for(let i=1 ; i<btns.length; i++){
+    btns[i].addEventListener("click", FilterImg);
+}
+
+function setActiveBtn(e){
+    btns.forEach(btn=>{
+        btn.classList.remove("btn-clicked")
+    });
+
+    e.target.classList.add("btn-clicked")
+
+}
+
+function FilterImg(e){
+    setActiveBtn(e)
+    imgs.forEach(img=>{
+        img.classList.remove("img-shrink");
+        img.classList.add("img-expand")
+
+        const imgType=parseInt(img.dataset.img)
+        const btnType=parseInt(e.target.dataset.btn);
+
+        if(imgType!==btnType){
+            img.classList.remove("img-expand")
+            img.classList.add("img-shrink")
+        }
+    });
+
+}
+btns[0].addEventListener("click",(e)=>{
+    setActiveBtn(e);
+    imgs.forEach(img=>{
+        img.classList.remove("img-shrink");
+        img.classList.add("img-expand");
+    });
+});
+
